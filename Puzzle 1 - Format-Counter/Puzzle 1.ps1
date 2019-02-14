947713fdcc295f34c59d58d08b1778086c01a342
Darrin Britton <brittod@wwu.edu>
2019-02-14 15:19:18 -0800

    formatting on puzzle 1 spec

--- Puzzle 1 - Format-Counter/Puzzle 1.ps1
+++ Puzzle 1 - Format-Counter/Puzzle 1.ps1
@@ -1,17 +1,37 @@
 https://ironscripter.us/iron-scripter-2019-is-coming/
 
 The Challenge
-The Get-Counter cmdlet is very useful in obtaining performance counter information. Unfortunately, the Chairman feels the output is less than friendly. Especially if you wish to consume the output such as with additional filtering, exporting to a json file or writing to a database.
 
-Your challenge is to create a PowerShell function that takes the output from Get-Counter and transforms it into a more user friendly object. Each counter sample should be a separate object with these properties:
+The Get-Counter cmdlet is very useful in obtaining performance counter information. Unfortunately, the Chairman feels the
+output is less than friendly. Especially if you wish to consume the output such as with additional filtering, exporting
+to a json file or writing to a database.
+
+Your challenge is to create a PowerShell function that takes the output from Get-Counter and transforms it into a more 
+user friendly object. Each counter sample should be a separate object with these properties:
 
 Timestamp
 Computername
 CounterSet
 Counter
 Value
-You are free to use whatever property names you want as long as it is clear what the values represent. A sample might look like this:
 
+You are free to use whatever property names you want as long as it is clear what the values represent. 
+
+A sample might look like this:
+
+PS C:\> Get-Counter -computername server01 | MyFunction
+
+Datetime    : 2/6/2019 1:34:30 PM
+Computername: SERVER01
+Counterset  : memory
+Counter     : cache faults/sec
+Value       : 0
+
+Datetime    : 2/6/2019 1:34:30 PM
+Computername: SERVER01
+Counterset  : physicaldisk(_total)
+Counter     : % disk time
+Value       : 5.63389896275325
 
 PS C:\> Get-Counter -computername server01 | MyFunction
 
@@ -27,21 +47,9 @@ Counterset  : physicaldisk(_total)
 Counter     : % disk time
 Value       : 5.63389896275325
 
-PS C:\> Get-Counter -computername server01 | MyFunction
-
-Datetime    : 2/6/2019 1:34:30 PM
-Computername: SERVER01
-Counterset  : memory
-Counter     : cache faults/sec
-Value       : 0
-
-Datetime    : 2/6/2019 1:34:30 PM
-Computername: SERVER01
-Counterset  : physicaldisk(_total)
-Counter     : % disk time
-Value       : 5.63389896275325
 
 Advanced Challenge
+
 If you want to really challenge yourself, define a custom type and format view that gives you a result like this:
 
 
@@ -70,5 +78,6 @@ SERVER01     memory                                                      % commi
 SERVER01     memory                                                      cache faults/sec          9.98958286299047
 SERVER01     physicaldisk(_total)                                        % disk time               0.06629201377444
 SERVER01     physicaldisk(_total)                                        current disk queue length                0
+
 
 All the information you need to know is included in this post. The Chairman will share a possible solution within the week. Good Luck and Good Scripting
